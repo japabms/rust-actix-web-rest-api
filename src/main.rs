6607cf2089abf0ded29b561ad6eef12cefe62411
japabms <matheuscorreaosu@gmail.com>
2023-06-11 21:47:29 -0300

    removendo rota para manipular o db e adicionando funcionalidade para pegar o path absoluto do openapi.json

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ pub mod services;
 pub mod api_docs;
 
 
+
+use api_docs::get_open_api_json_path;
 use controllers::{
     artigo_controller::*, atividade_controller::*, categoria_controller::*, curso_controller::*,
     evento_controller::*, inscrito_controller::*, noticia_controller::*,
@@ -14,7 +16,7 @@ use controllers::{
 
 
 use actix_cors::Cors;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use db::{establish_connection, get_pool, run_migrations};
 
 use utoipa::OpenApi;
@@ -28,20 +30,14 @@ use api_docs::update_api_docs;
 
 
 //Ignorar
-#[get("/db/{table_name}")]
-async fn serial_util(table: web::Path<String>) -> impl Responder {
-    let conn = establish_connection();
-
-    match db::reset_serial(table.into_inner().as_str(), "id", conn) {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::BadRequest().finish(),
-    }
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    //loading api definition
+    //i dont know if it works for windows.
+    let swagger_docs = get_open_api_json_path()?;
     update_api_docs()?;
 
     // rodando as migrações
@@ -57,7 +53,7 @@ async fn main() -> std::io::Result<()> {
                 Cors::default()
                     .allow_any_origin()
                     .allow_any_method()
-                    .allow_any_header(),
+                    .allow_any_header()
             )
             .wrap(actix_web::middleware::Logger::default())
 
@@ -69,9 +65,8 @@ async fn main() -> std::io::Result<()> {
             .configure(init_inscrito_routes)
             .configure(init_noticia_routes)
 
-            .service(serial_util)
             .service(SwaggerUi::new("/swagger-ui/{_:.*}").url(
-                "/home/victor/workspace/rust/cesc-api/openapi.json",
+                swagger_docs.clone(),
                 ApiDoc::openapi(),
             ))
     })
